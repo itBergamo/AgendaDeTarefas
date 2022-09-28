d2ec2bfe9b11edf970f97db57255dae5a1c6aa31
itBergamo <isabelathaisb@gmail.com>
2022-09-28 14:52:53 -0300

    Adicionar arquivos de projeto.

---
+++ AgendaDeTarefas/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AgendaDeTarefas;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgendaDeTarefas.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet <Task> Tarefa { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=DBAppSample;Integrated Security=True");
+        }
+    }
+}
---
+++ AgendaDeTarefas/Models/Tarefa.cs
@@ -0,0 +1,14 @@
+﻿namespace AgendaDeTarefas.Models
+{
+    public class Tarefa
+    { 
+    public int Tarefa_id { get; set; }
+    public string Titulo { get; set; }
+    public string Descricao { get; set; }
+    public DateOnly DataTarefa { get; set; }
+    public TimeOnly Inicio { get; set; }
+    public TimeOnly Fim { get; set; }
+    public string Prioridade { get; set; }
+    public Boolean finalizada { get; set; }
+    }
+}
